2357271358c13585702404c75a7fc77b1551446d
jeymarherrera <jeymarherrera@gmail.com>
2022-11-08 22:28:22 -0500

    Add project files.

---
+++ crud_vb/frmRegistros.Designer.vb
@@ -0,0 +1,395 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmRegistros
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtFecha = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.cbxGenero = New System.Windows.Forms.ComboBox()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.dgvRegistros = New System.Windows.Forms.DataGridView()
+        Me.ColumnId = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnNombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnApellido = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnFecha = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnGenero = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnTelefono = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnDireccion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.ErrorProviderValidar = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvRegistros, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProviderValidar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(176, 58)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(333, 23)
+        Me.txtNombre.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.txtFecha)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.txtApellido)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtDireccion)
+        Me.GroupBox1.Controls.Add(Me.txtTelefono)
+        Me.GroupBox1.Controls.Add(Me.txtId)
+        Me.GroupBox1.Controls.Add(Me.cbxGenero)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Location = New System.Drawing.Point(51, 49)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(532, 241)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos del Contacto"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(21, 116)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(119, 15)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Fecha de Nacimiento"
+        '
+        'txtFecha
+        '
+        Me.txtFecha.Location = New System.Drawing.Point(176, 116)
+        Me.txtFecha.Name = "txtFecha"
+        Me.txtFecha.Size = New System.Drawing.Size(333, 23)
+        Me.txtFecha.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(22, 87)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 15)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Apellido"
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(176, 87)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(333, 23)
+        Me.txtApellido.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 145)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 15)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Genero"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 203)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 15)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Direccion"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 174)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Telefono"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Identificacion"
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(176, 203)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(333, 23)
+        Me.txtDireccion.TabIndex = 5
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(176, 174)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(333, 23)
+        Me.txtTelefono.TabIndex = 4
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(176, 29)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(333, 23)
+        Me.txtId.TabIndex = 3
+        '
+        'cbxGenero
+        '
+        Me.cbxGenero.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbxGenero.FormattingEnabled = True
+        Me.cbxGenero.Items.AddRange(New Object() {"Masculino", "Femenino"})
+        Me.cbxGenero.Location = New System.Drawing.Point(176, 145)
+        Me.cbxGenero.Name = "cbxGenero"
+        Me.cbxGenero.Size = New System.Drawing.Size(333, 23)
+        Me.cbxGenero.TabIndex = 2
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(612, 110)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAgregar.TabIndex = 3
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'dgvRegistros
+        '
+        Me.dgvRegistros.AllowUserToAddRows = False
+        Me.dgvRegistros.AllowUserToDeleteRows = False
+        Me.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvRegistros.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColumnId, Me.ColumnNombre, Me.ColumnApellido, Me.ColumnFecha, Me.ColumnGenero, Me.ColumnTelefono, Me.ColumnDireccion})
+        Me.dgvRegistros.Location = New System.Drawing.Point(26, 357)
+        Me.dgvRegistros.Name = "dgvRegistros"
+        Me.dgvRegistros.ReadOnly = True
+        Me.dgvRegistros.RowTemplate.Height = 25
+        Me.dgvRegistros.Size = New System.Drawing.Size(738, 238)
+        Me.dgvRegistros.TabIndex = 4
+        '
+        'ColumnId
+        '
+        Me.ColumnId.HeaderText = "Identificacion"
+        Me.ColumnId.Name = "ColumnId"
+        Me.ColumnId.ReadOnly = True
+        '
+        'ColumnNombre
+        '
+        Me.ColumnNombre.HeaderText = "Nombre"
+        Me.ColumnNombre.Name = "ColumnNombre"
+        Me.ColumnNombre.ReadOnly = True
+        '
+        'ColumnApellido
+        '
+        Me.ColumnApellido.HeaderText = "Apellido"
+        Me.ColumnApellido.Name = "ColumnApellido"
+        Me.ColumnApellido.ReadOnly = True
+        '
+        'ColumnFecha
+        '
+        Me.ColumnFecha.HeaderText = "Fecha de Nacimiento"
+        Me.ColumnFecha.Name = "ColumnFecha"
+        Me.ColumnFecha.ReadOnly = True
+        '
+        'ColumnGenero
+        '
+        Me.ColumnGenero.HeaderText = "Genero"
+        Me.ColumnGenero.Name = "ColumnGenero"
+        Me.ColumnGenero.ReadOnly = True
+        '
+        'ColumnTelefono
+        '
+        Me.ColumnTelefono.HeaderText = "Telefono"
+        Me.ColumnTelefono.Name = "ColumnTelefono"
+        Me.ColumnTelefono.ReadOnly = True
+        '
+        'ColumnDireccion
+        '
+        Me.ColumnDireccion.HeaderText = "Direccion"
+        Me.ColumnDireccion.Name = "ColumnDireccion"
+        Me.ColumnDireccion.ReadOnly = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(364, 324)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(103, 15)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Lista de Contactos"
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Enabled = False
+        Me.btnModificar.Location = New System.Drawing.Point(612, 139)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnModificar.TabIndex = 11
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Enabled = False
+        Me.btnEliminar.Location = New System.Drawing.Point(612, 168)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 12
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(612, 197)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 13
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(612, 226)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 14
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'ErrorProviderValidar
+        '
+        Me.ErrorProviderValidar.ContainerControl = Me
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(301, 18)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(250, 17)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "SISTEMA DE REGISTROS DE CONTACTOS"
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(612, 81)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 18
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'frmRegistros
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(790, 607)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.dgvRegistros)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "frmRegistros"
+        Me.Text = "Sistema de Registros"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.dgvRegistros, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProviderValidar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents cbxGenero As ComboBox
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents dgvRegistros As DataGridView
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents ErrorProviderValidar As ErrorProvider
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtFecha As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents ColumnId As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnNombre As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnApellido As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnFecha As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnGenero As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnTelefono As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnDireccion As DataGridViewTextBoxColumn
+    Friend WithEvents Label9 As Label
+    Friend WithEvents btnNuevo As Button
+End Class
---
+++ crud_vb/frmRegistros.vb
@@ -0,0 +1,252 @@
+﻿Imports System.IO 'para los archivos'
+Imports System.Security.Policy
+
+Public Class frmRegistros
+
+#Region "Variables Globales"
+    Dim identificacion, nombre, apellido, fecha_nac, genero, telefono, direccion As String
+    Dim validarCampos, validarID As Boolean
+    Dim Fila As String
+#End Region
+
+    'FUNCIONES PRINCIPALES'
+#Region "Boton Nuevo"
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        LimpiarCampos()
+        ActivarBotones()
+    End Sub
+#End Region
+
+#Region "Boton Agregar"
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        identificacion = txtId.Text
+        nombre = txtNombre.Text
+        apellido = txtApellido.Text
+        fecha_nac = txtFecha.Text
+        genero = cbxGenero.Text
+        telefono = txtTelefono.Text
+        direccion = txtDireccion.Text
+
+        validarCampos = ValidarDatos(identificacion, nombre, apellido, fecha_nac, genero, telefono, direccion)
+
+        If validarCampos = True Then
+            validarID = ValidarIdentificacion(identificacion)
+            If validarID = True Then
+                dgvRegistros.Rows.Add(identificacion, nombre, apellido, fecha_nac, genero, telefono, direccion)
+                MsgBox("Contacto registrado exitosamente", MsgBoxStyle.Information, "Informacion")
+                LimpiarCampos()
+            End If
+        End If
+    End Sub
+#End Region
+
+#Region "Boton Modificar"
+    'Boton Modificar'
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        identificacion = txtId.Text
+        nombre = txtNombre.Text
+        apellido = txtApellido.Text
+        fecha_nac = txtFecha.Text
+        genero = cbxGenero.Text
+        telefono = txtTelefono.Text
+        direccion = txtDireccion.Text
+
+        validarCampos = ValidarDatos(identificacion, nombre, apellido, fecha_nac, genero, telefono, direccion)
+        Fila = dgvRegistros.CurrentRow.Index
+        validarID = True
+        If validarCampos = True Then
+            If identificacion <> dgvRegistros.Rows(Fila).Cells("ColumnId").Value Then
+                validarID = ValidarIdentificacion(identificacion)
+            End If
+
+            If validarID = True Then
+                dgvRegistros.Rows(Fila).Cells("ColumnId").Value = identificacion
+                dgvRegistros.Rows(Fila).Cells("ColumnNombre").Value = nombre
+                dgvRegistros.Rows(Fila).Cells("ColumnApellido").Value = apellido
+                dgvRegistros.Rows(Fila).Cells("ColumnFecha").Value = fecha_nac
+                dgvRegistros.Rows(Fila).Cells("ColumnGenero").Value = genero
+                dgvRegistros.Rows(Fila).Cells("ColumnTelefono").Value = telefono
+                dgvRegistros.Rows(Fila).Cells("ColumnDireccion").Value = direccion
+
+
+                MsgBox("Modificacion exitosamente", MsgBoxStyle.Information, "Informacion")
+                ActivarBotones()
+                LimpiarCampos()
+            Else
+
+            End If
+        End If
+    End Sub
+
+    'Seleccionar fila a modificar en el dgv'
+    Private Sub dgvRegistros_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRegistros.CellDoubleClick
+        Fila = dgvRegistros.CurrentRow.Index
+        txtNombre.Text = dgvRegistros.Rows(Fila).Cells("ColumnNombre").Value
+        txtId.Text = dgvRegistros.Rows(Fila).Cells("ColumnID").Value
+        txtTelefono.Text = dgvRegistros.Rows(Fila).Cells("ColumnTelefono").Value
+        txtDireccion.Text = dgvRegistros.Rows(Fila).Cells("ColumnDireccion").Value
+        cbxGenero.Text = dgvRegistros.Rows(Fila).Cells("ColumnGenero").Value
+        DesactivarBotones()
+    End Sub
+
+#End Region
+
+#Region "Boton Eliminar"
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim confirmacion As String
+        confirmacion = MsgBox("Seguro que desea eliminar este contacto?", MsgBoxStyle.YesNo, "Informacion")
+        If confirmacion = vbYes Then
+            Fila = dgvRegistros.CurrentRow.Index
+            dgvRegistros.Rows.Remove(dgvRegistros.Rows(Fila))
+            MsgBox("Eliminacion exitosa", MsgBoxStyle.Information, "Informacion")
+            ActivarBotones()
+            LimpiarCampos()
+        End If
+    End Sub
+#End Region
+
+#Region "Boton Guardar"
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        GuardarDatos()
+    End Sub
+#End Region
+
+#Region "Boton Salir"
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Dim confirmacion As String
+        confirmacion = MsgBox("Antes de salir. Deseas guardar los datos?", MsgBoxStyle.YesNo, "Informacion")
+        If confirmacion = vbYes Then
+            GuardarDatos()
+        End If
+        Me.Close()
+    End Sub
+#End Region
+
+    'FUNCIONES EXTRAS'
+
+#Region "Validar Campos Vacios"
+    Function ValidarDatos(id As String, name As String, lastname As String, birth As String, gender As String, phone As String, address As String) As Boolean
+        If id = "" Then
+            MsgBox("Ingrese la identificacion, no puede ir vacio este campo", MsgBoxStyle.Critical, "Informacion")
+            ErrorProviderValidar.SetError(txtId, "Ingrese identificacion")
+            validarCampos = False
+        ElseIf name = "" Then
+            MsgBox("Ingrese el nombre, no puede ir vacio este campo", MsgBoxStyle.Critical, "Informacion")
+            ErrorProviderValidar.SetError(txtNombre, "Ingrese nombre")
+            validarCampos = False
+        ElseIf lastname = "" Then
+            MsgBox("Ingrese el apellido, no puede ir vacio este campo", MsgBoxStyle.Critical, "Informacion")
+            ErrorProviderValidar.SetError(txtNombre, "Ingrese apellido")
+            validarCampos = False
+        ElseIf birth = "" Then
+            MsgBox("Ingrese la fecha de nacimiento, no puede ir vacio este campo", MsgBoxStyle.Critical, "Informacion")
+            ErrorProviderValidar.SetError(txtNombre, "Ingrese fecha de nacimiento")
+            validarCampos = False
+        ElseIf gender = "" Then
+            MsgBox("Seleccione el genero, no puede ir sin seleccion", MsgBoxStyle.Critical, "Informacion")
+            ErrorProviderValidar.SetError(cbxGenero, "Seleccione genero")
+            validarCampos = False
+        ElseIf phone = "" Then
+            MsgBox("Ingrese el telefono, no puede ir vacio este campo", MsgBoxStyle.Critical, "Informacion")
+            ErrorProviderValidar.SetError(txtTelefono, "Ingrese telefono")
+            validarCampos = False
+        ElseIf address = "" Then
+            MsgBox("Ingrese la direccion, no puede ir vacio este campo", MsgBoxStyle.Critical, "Informacion")
+            ErrorProviderValidar.SetError(txtDireccion, "Ingrese direccion")
+            validarCampos = False
+        Else
+            validarCampos = True
+            ErrorProviderValidar.SetError(txtId, "")
+            ErrorProviderValidar.SetError(txtNombre, "")
+            ErrorProviderValidar.SetError(txtApellido, "")
+            ErrorProviderValidar.SetError(txtFecha, "")
+            ErrorProviderValidar.SetError(cbxGenero, "")
+            ErrorProviderValidar.SetError(txtTelefono, "")
+            ErrorProviderValidar.SetError(txtDireccion, "")
+        End If
+        Return validarCampos
+    End Function
+
+#End Region
+
+#Region "Validar Contacto Existente"
+    Function ValidarIdentificacion(id As String) As Boolean
+        validarID = True
+        For y = 0 To dgvRegistros.RowCount - 1
+            If id = dgvRegistros.Rows(y).Cells("ColumnId").Value Then
+                MsgBox("Contacto Existente", MsgBoxStyle.Critical, "Informacion")
+                ErrorProviderValidar.SetError(txtId, "Ingrese una identificacion nueva")
+                validarID = False
+                Exit For
+            Else
+                ErrorProviderValidar.SetError(txtId, "")
+                validarID = True
+            End If
+        Next
+        Return validarID
+    End Function
+#End Region
+
+#Region "Limpiar Campos"
+    Private Sub LimpiarCampos()
+        txtId.Text = ""
+        txtNombre.Text = ""
+        txtApellido.Text = ""
+        txtFecha.Text = ""
+        txtTelefono.Text = ""
+        txtDireccion.Text = ""
+    End Sub
+#End Region
+
+#Region "Activar y Desactivar botones"
+    Private Sub ActivarBotones()
+        btnAgregar.Enabled = True
+        btnGuardar.Enabled = True
+        btnModificar.Enabled = False
+        btnEliminar.Enabled = False
+    End Sub
+
+    Private Sub DesactivarBotones()
+        btnAgregar.Enabled = False
+        btnGuardar.Enabled = False
+        btnModificar.Enabled = True
+        btnEliminar.Enabled = True
+    End Sub
+#End Region
+
+#Region "Escribir Archivo"
+    Private Sub GuardarDatos()
+        Dim archivo_escritura As StreamWriter
+        Dim linea As String
+        archivo_escritura = New StreamWriter("./datos.txt")
+        With dgvRegistros
+            For y = 0 To .RowCount - 1
+                linea = .Rows(y).Cells("ColumnId").Value & ";" &
+                    .Rows(y).Cells("ColumnNombre").Value & ";" &
+                    .Rows(y).Cells("ColumnApellido").Value & ";" &
+                    .Rows(y).Cells("ColumnFecha").Value & ";" &
+                    .Rows(y).Cells("ColumnGenero").Value & ";" &
+                    .Rows(y).Cells("ColumnTelefono").Value & ";" &
+                    .Rows(y).Cells("ColumnDireccion").Value & ";"
+                archivo_escritura.WriteLine(linea)
+            Next
+            MsgBox("Datos guardados exitosamente", MsgBoxStyle.Information, "Informacion")
+        End With
+        archivo_escritura.Close()
+    End Sub
+#End Region
+
+#Region "Leer Archivo"
+    Private Sub frmRegistros_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim archivo_lectura As StreamReader
+        archivo_lectura = New StreamReader("./datos.txt")
+        While Not archivo_lectura.EndOfStream
+            Dim cadena As String = archivo_lectura.ReadLine
+            Dim lectura As String() = cadena.Split(New Char() {";"})
+            dgvRegistros.Rows.Add(lectura)
+        End While
+        archivo_lectura.Close()
+    End Sub
+#End Region
+
+End Class
